fd1cd9dac0b07f45d53bfd0062fb16be47e07364
ohansemmanuel <ohansemmanuel@outlook.com>
2023-04-17 17:35:38 +0200

    ✨ feat: setup tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,44 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("./colors.cjs");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: [
+          "Optimistic Display",
+          "-apple-system",
+          ...defaultTheme.fontFamily.sans,
+        ],
+        text: [
+          "Optimistic Text",
+          "-apple-system",
+          ...defaultTheme.fontFamily.sans,
+        ],
+        mono: ['"Source Code Pro"', ...defaultTheme.fontFamily.mono],
+      },
+      fontSize: {
+        "6xl": "52px",
+        "5xl": "40px",
+        "4xl": "32px",
+        "3xl": "28px",
+        "2xl": "24px",
+        xl: "20px",
+        lg: "17px",
+        base: "15px",
+        sm: "13px",
+        xs: "11px",
+        code: "calc(1em - 20%)",
+      },
+      lineHeight: {
+        base: "30px",
+        large: "38px",
+        xl: "1.15",
+      },
+      colors,
+    },
+  },
+  plugins: [],
+};
