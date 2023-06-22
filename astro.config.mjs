1224c84ddb0264d9f916233a957ee2bbe9788bf6
ohansemmanuel <ohansemmanuel@outlook.com>
2023-06-22 08:22:55 +0200

    🔥 feat: remove integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [],
 });
